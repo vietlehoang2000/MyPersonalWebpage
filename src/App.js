8daf43faa8359617f0df57ca4d7ca2db9d5e3bb0
Nam Hoang <nam.hoang@aeris.net>
2021-11-29 17:03:54 +0700

    remove download cv

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import "./App.scss";
+import './App.scss';
 
-import Home from "./components/home";
+import Home from './components/home';
 
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
-import Logo from "./utilities/image/Logo.svg";
-import LogoWhite from "./utilities/image/logo-white.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from './utilities/image/Logo.svg';
+import LogoWhite from './utilities/image/logo-white.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Link } from "react-scroll";
+import { Link } from 'react-scroll';
 
-import CV from "./utilities/CV/CV.pdf";
-import { useState } from "react";
+import CV from './utilities/CV/CV.pdf';
+import { useState } from 'react';
 
 function App() {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState('dark');
 
   function changeTheme() {
-    document.documentElement.classList.add("transition");
+    document.documentElement.classList.add('transition');
     window.setTimeout(() => {
-      document.documentElement.classList.remove("transition");
+      document.documentElement.classList.remove('transition');
     }, 1000);
-    if (theme === "dark") {
-      setTheme("light");
+    if (theme === 'dark') {
+      setTheme('light');
     }
-    if (theme === "light") {
-      setTheme("dark");
+    if (theme === 'light') {
+      setTheme('dark');
     }
   }
 
@@ -41,35 +41,35 @@ function App() {
     <>
       <Navbar
         className={`nav--home animate__animated animate__fadeIn animate__delay-4s ${theme}`}
-        fixed="top"
-        variant="dark"
+        fixed='top'
+        variant='dark'
       >
         <Container className={theme}>
-          <Navbar.Brand href="">
-            <Link to="home">
+          <Navbar.Brand href=''>
+            <Link to='home'>
               <img
-                className="imageSpinNav"
-                alt="logo"
-                src={theme === "dark" ? Logo : LogoWhite}
+                className='imageSpinNav'
+                alt='logo'
+                src={theme === 'dark' ? Logo : LogoWhite}
                 onClick={() => changeTheme()}
               ></img>
             </Link>
           </Navbar.Brand>
           <Nav>
-            <Link to="home" spy={true} offset={-100} isDynamic={true}>
+            <Link to='home' spy={true} offset={-100} isDynamic={true}>
               Home
             </Link>
-            <Link to="about" spy={true} offset={-100} isDynamic={true}>
+            <Link to='about' spy={true} offset={-100} isDynamic={true}>
               About
             </Link>
-            <Link to="porfolio" spy={true} offset={-100} isDynamic={true}>
+            <Link to='porfolio' spy={true} offset={-100} isDynamic={true}>
               Portfolio
             </Link>
-            <Link to="contact" spy={true} offset={-100} isDynamic={true}>
+            <Link to='contact' spy={true} offset={-100} isDynamic={true}>
               Contact
             </Link>
-            <a href={CV} download="Le-Hoang-Viet-CV">
-              <Button className="button--cv" size="sm">
+            <a download='Le-Hoang-Viet-CV'>
+              <Button className='button--cv' size='sm'>
                 My CV
               </Button>
             </a>
--- src/components/burgermenu.js
+++ src/components/burgermenu.js
@@ -1,49 +1,64 @@
-import Offcanvas from "react-bootstrap/Offcanvas";
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-import Logo from "../utilities/image/Logo.svg";
+import Logo from '../utilities/image/Logo.svg';
 
-import {IoCloseSharp} from 'react-icons/io5'
+import { IoCloseSharp } from 'react-icons/io5';
 
-import Nav from 'react-bootstrap/Nav'
-import Button from "react-bootstrap/Button";
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
-import { Link } from "react-scroll";
+import { Link } from 'react-scroll';
 
-import CV from '../utilities/CV/CV.pdf'
+import CV from '../utilities/CV/CV.pdf';
 
 export default function BurgerMenu({ show, handleClose }) {
   return (
     <>
       <Offcanvas show={show} onHide={handleClose}>
-        <div className="burger__button-close text-end" onClick={handleClose}>
-        <IoCloseSharp></IoCloseSharp>
+        <div className='burger__button-close text-end' onClick={handleClose}>
+          <IoCloseSharp></IoCloseSharp>
         </div>
-        <div className="burger__nav">
-        <Offcanvas.Title className="text-center">
-          <img alt="logo" className="imageSpinNav" src={Logo}></img>
-        </Offcanvas.Title>
-        <Offcanvas.Body>
-        <Nav className="burger__nav--link row">
-        <Link to="home" spy={true}  offset={-100} isDynamic={true} >
-              Home
-            </Link>
-            <Link to="about" spy={true}  offset={-100} isDynamic={true}>
-              About
-            </Link>
-            <Link to="porfolio" spy={true}  offset={-100} isDynamic={true}>
-              Portfolio
-            </Link>
-            <Link to="contact" spy={true}  offset={-100} isDynamic={true}>
-            Contact
-            </Link>
-            <a href={CV} download="Le-Hoang-Viet-CV"><Button onClick={()=> window.open("https://www.topcv.vn/xem-cv/XANeV1UCBgMGBgcGVAVWV1xRDQECUQlUDFNWAA2ae7?utm_source=link_i_topcv&utm_campaign=link_i_topcv&utm_medium=link_i_topcv")} className="button--cv col-4 mx-auto" size="sm">
+        <div className='burger__nav'>
+          <Offcanvas.Title className='text-center'>
+            <img alt='logo' className='imageSpinNav' src={Logo}></img>
+          </Offcanvas.Title>
+          <Offcanvas.Body>
+            <Nav className='burger__nav--link row'>
+              <Link to='home' spy={true} offset={-100} isDynamic={true}>
+                Home
+              </Link>
+              <Link to='about' spy={true} offset={-100} isDynamic={true}>
+                About
+              </Link>
+              <Link to='porfolio' spy={true} offset={-100} isDynamic={true}>
+                Portfolio
+              </Link>
+              <Link to='contact' spy={true} offset={-100} isDynamic={true}>
+                Contact
+              </Link>
+              {/* <a download='Le-Hoang-Viet-CV'> */}
+              <a>
+                <Button
+                  // onClick={() =>
+                  //   window.open(
+                  //     'https://www.topcv.vn/xem-cv/XANeV1UCBgMGBgcGVAVWV1xRDQECUQlUDFNWAA2ae7?utm_source=link_i_topcv&utm_campaign=link_i_topcv&utm_medium=link_i_topcv'
+                  //   )
+                  // }
+                  className='button--cv col-4 mx-auto'
+                  size='sm'
+                >
                   My CV
-            </Button></a>
-          </Nav>
-        <h5 className="text-center mt-3">©2021 Vivian</h5>
-        <a className="phone" href="tel:+81972136868"><h5 className="text-center">+81 0972136868</h5></a>
-        <a className="mail" href="mailto:vitll474@gmail.com"><h5 className="text-center">vietlehoang2000@gmail.com</h5></a>
-        </Offcanvas.Body>
+                </Button>
+              </a>
+            </Nav>
+            <h5 className='text-center mt-3'>©2021 Vivian</h5>
+            <a className='phone' href='tel:+81972136868'>
+              <h5 className='text-center'>+81 0972136868</h5>
+            </a>
+            <a className='mail' href='mailto:vitll474@gmail.com'>
+              <h5 className='text-center'>vietlehoang2000@gmail.com</h5>
+            </a>
+          </Offcanvas.Body>
         </div>
       </Offcanvas>
     </>
