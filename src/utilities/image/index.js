5bdf3b278db4f7f3a0ee130d476fd1ef6887b664
vietlehoang2000 <vietlehoang2000@gmail.com>
2023-03-14 17:53:24 +1100

    feat: add projects

--- src/utilities/image/index.js
+++ src/utilities/image/index.js
@@ -1,6 +1,6 @@
-import  Apple1 from './appleDetail/apple1.png'
-import  Apple2 from './appleDetail/apple2.png'
-import  Apple3 from './appleDetail/apple3.png'
+import Apple1 from './appleDetail/apple1.png'
+import Apple2 from './appleDetail/apple2.png'
+import Apple3 from './appleDetail/apple3.png'
 import Coca1 from './cocaDetail/coca1.png'
 import Coca2 from './cocaDetail/coca2.png'
 import Coca3 from './cocaDetail/coca3.png'
@@ -12,4 +12,10 @@ import PeopleHealth1 from './peopleHealthDetail/peopleHealth1.png'
 import PeopleHealth2 from './peopleHealthDetail/peopleHealth2.png'
 import PeopleHealth3 from './peopleHealthDetail/peopleHealth3.png'
 import PeopleHealth4 from './peopleHealthDetail/peopleHealth4.png'
-export {Apple1,Apple2,Apple3,Coca1,Coca2,Coca3,Mammas1,Mammas2,Mammas3,Mammas4,PeopleHealth1,PeopleHealth2,PeopleHealth3,PeopleHealth4}
+import DashboardAerport from "./dashboard_aerport.png"
+import DashboardAerport2 from "./dashboard_aerport_1.png"
+import Parkfree from "./parkFree.png"
+import DND from "./DND.png"
+import DND2 from "./DND2.png"
+
+export { Apple1, Apple2, Apple3, Coca1, Coca2, Coca3, Mammas1, Mammas2, Mammas3, Mammas4, PeopleHealth1, PeopleHealth2, PeopleHealth3, PeopleHealth4, DashboardAerport, DashboardAerport2, Parkfree, DND, DND2 }
